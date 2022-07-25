aa950e67635f4a3ebdf9e27ec461303310f30a1f
İsa Aras <arasisa6128@gmail.com>
2022-07-25 23:02:07 +0300

    init

---
+++ HaftalıkWork/CreditCardApplication/CreditCardApplication/CreditCart.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CreditCardApplication
+{
+    class CreditCart
+    {
+
+        private String number;
+        private String holderName;
+        private string expirationDate;
+        private int ccv;
+
+      
+
+
+        public CreditCart(string number, string holderName,string expirationDate, int ccv)
+        {
+
+
+       
+
+            this.number = number;
+            this.holderName = holderName;
+            this.expirationDate = expirationDate;
+            this.ccv = ccv;
+
+
+        }
+
+ 
+
+        public void setInfos(string number)
+        {
+            this.number = number;
+           
+        }
+
+        public void setInfos(string number, string holderName,string expirationDate)
+        {
+            this.number = number;
+            this.expirationDate = expirationDate;
+            this.holderName = holderName;
+        
+
+        }
+
+
+
+
+
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+
+            set
+            {
+                number = value;
+            }
+        }
+
+        public string HolderName
+        {
+            get
+            {
+                return holderName;
+            }
+
+            set
+            {
+                holderName = value;
+            }
+        }
+
+        public string ExpirationDate
+        {
+            get
+
+                {
+                return expirationDate;
+            }
+
+            set
+            {
+                expirationDate = value;
+            }
+        }
+
+
+        public int CCv
+        {
+            get
+            {
+                return ccv;
+            }
+
+            set
+            {
+                ccv = value;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public DateTime ConvertToDateTime()
+        {
+
+            DateTime dtFinaldate; string sDateTime;
+            try
+            {
+                dtFinaldate = Convert.ToDateTime(expirationDate);
+
+                if (dtFinaldate.Year <= 2022)
+                {
+                  
+                    Console.WriteLine("Kart Tarihi Geçersiz Lütfen Tarihi Değiştirin.");
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Kart Tarihi Geçerli Kart Onaylandı");
+                }
+
+            }
+            catch (Exception e)
+            {
+                string[] sDate = expirationDate.Split('/');
+                sDateTime = sDate[0] + '/' + sDate[1] + '/' + sDate[2];
+                dtFinaldate = Convert.ToDateTime(sDateTime);
+
+              
+
+            }
+            return dtFinaldate;
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        public void cardControl()
+        {
+            try
+            {
+                if (number.Length != 16)
+                {
+                    throw new CreditCartException("Kart Numarası Geçersiz Lütfen 16 Haneli Olacak Şekilde Tanımlayınız. !!!");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Geçerli Kart Numarası Doğru Tanımlanmıştır.");
+
+                }
+                return;
+               
+            }
+            catch (CreditCartException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+      public class CreditCartException : System.Exception
+        {
+            public CreditCartException()
+                : base()
+            { }
+
+            public CreditCartException(String message)
+                : base(message)
+
+            { }
+
+            public CreditCartException(String message, Exception innerException)
+                : base(message, innerException)
+            { }
+
+            protected CreditCartException(SerializationInfo info, StreamingContext context)
+                : base(info, context)
+            { }
+        }
+
+
+        public String getInfo()
+        {
+
+            return "Kart No :"+number+"\t"+"Ad Soyad :"+holderName+"\t"+"Tarih :"+ConvertToDateTime().ToString().TrimEnd('0',':')+ "\t"+"Güvenlik Kodu :"+ccv;
+          
+        }
+
+
+      
+
+    }
+}
---
+++ HaftalıkWork/CreditCardApplication/CreditCardApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace CreditCardApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            CreditCart creditCart = new CreditCart("1111111111111111", "İsa Aras","19/12/23", 789);
+            creditCart.cardControl();
+            Console.WriteLine(creditCart.getInfo() + "\n \n");
+
+
+            creditCart.setInfos("232133232313","Fatih Yol","09/12/22");
+            creditCart.cardControl();
+            Console.WriteLine(creditCart.getInfo() + "\n \n");
+
+
+
+
+
+
+            creditCart.setInfos("555555555555435355555","Ramazan Çalışkan","02/12/19");
+            creditCart.cardControl();
+            Console.WriteLine(creditCart.getInfo() + "\n \n");
+
+
+
+
+        }
+
+    }
+}
