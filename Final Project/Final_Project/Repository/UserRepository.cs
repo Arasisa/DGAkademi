322c4c930a6598b18af50211502cfb4958bd27a2
İsa Aras <arasisa6128@gmail.com>
2022-08-28 19:31:06 +0300

    init

---
+++ Final Project/Final_Project/Repository/UserRepository.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MovieApp.API.ApplicationOptions;
+using MovieApp.API.Data;
+using MovieApp.API.Models;
+using MovieApp.API.Repository.IRepository;
+using PasswordHashingLibrary.Helper.PasswordHasher;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly AppSettings _appSettings;
+
+        public UserRepository(ApplicationDbContext dbContext, IOptions<AppSettings> appSettings)
+        {
+            _dbContext = dbContext;
+            _appSettings = appSettings.Value;
+        }
+        public UserModel Authenticate(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                return null;
+
+            var user = _dbContext.Users.SingleOrDefault(x => x.UserName == username);
+
+            //check if username exists
+            if (user == null)
+                return null;
+
+            //checks if password is correct
+            if (!PasswordHash.VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            //if user was found generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.UserId.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials
+                (new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            //authentication successful
+            return user;
+        }
+
+        public UserModel Create(UserModel user, string password)
+        {
+            UserModel userObj = new UserModel
+            {
+                UserName = user.UserName,
+                Password = user.Password,
+                Role = "Customer",
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+            };
+
+            //validation
+            if (string.IsNullOrWhiteSpace(password))
+                throw new AppException("Password is required");
+
+            if (_dbContext.Users.Any(x => x.UserName == user.UserName))
+                throw new AppException("Username \"" + user.UserName + "\" is already taken");
+
+            byte[] passwordHash, passwordSalt;
+            PasswordHash.CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            userObj.PasswordHash = passwordHash;
+            userObj.PasswordSalt = passwordSalt;
+
+            _dbContext.Users.Add(userObj);
+            _dbContext.SaveChanges();
+
+            return userObj;
+        }
+
+        public void Delete(int id)
+        {
+            var user = _dbContext.Users.Find(id);
+            if (user != null)
+            {
+                _dbContext.Users.Remove(user);
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            return _dbContext.Users;
+        }
+
+        public UserModel GetUserById(int id)
+        {
+            return _dbContext.Users.Find(id);
+        }
+
+        public void Update(UserModel user, string password = null)
+        {
+            var userParam = _dbContext.Users.Find(user.UserId);
+
+            if (userParam == null)
+                throw new AppException("User not found");
+
+            if (userParam.UserName != user.UserName)
+            {
+                //username has changed so check if the new username is already taken
+                if (_dbContext.Users.Any(x => x.UserName == userParam.UserName))
+                    throw new AppException("Username " + userParam.UserName + " is already taken");
+            }
+
+            //update user properties
+            user.FirstName = userParam.FirstName;
+            user.LastName = userParam.LastName;
+            user.UserName = userParam.UserName;
+            user.Email = userParam.Email;
+
+            //update password if it was entered
+            if (!string.IsNullOrWhiteSpace(password))
+            {
+                byte[] passwordHash, passwordSalt;
+                PasswordHash.CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+            }
+
+            _dbContext.Users.Update(user);
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Final Project/Final_Project/Startup.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MovieApp.API.ApplicationOptions;
+using MovieApp.API.Data;
+using MovieApp.API.Models.DTOs.MovieAppMapper;
+using MovieApp.API.Repository;
+using MovieApp.API.Repository.IRepository;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MovieApp.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddDbContext<ApplicationDbContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+            services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen();
+            services.AddScoped<IGenreRepository, GenreRepository>();
+            services.AddScoped<ISubGenreRepository, SubGenreRepository>();
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(MovieMapper));
+            services.AddControllers().AddNewtonsoftJson(s =>
+            s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+            services.AddControllers();
+
+            
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var desc in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json",
+                        desc.GroupName.ToUpperInvariant());
+                }
+                options.RoutePrefix = "";
+            });
+
+            app.UseRouting();
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
